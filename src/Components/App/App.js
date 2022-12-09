70895f6025870ae06f89278c305b75d05cc2636d
Cmndgrab <110107052+Cmndgrab@users.noreply.github.com>
2022-12-09 09:53:21 +0000

    please

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,29 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState, useContext, useEffect } from 'react';
+
+const url = process.env.REACT_APP_BACKEND_URL ?? "http://localhost:3000";
+
 function App() {
 
   const [list, setList] = useState([]);
 
   // Fetching shopping list data from shopping list API.
   useEffect(() => {
-    async function getShoppingList() {
+    async function getChristmasList() {
       const response = await fetch(`${url}/items`);
       const data = await response.json(response);
       console.log(data);
       setList(data.payload);
     }
-    getShoppingList();
+    getChristmasList();
   }, []);
 
+  async function addToList(newListItem) {
+    //This function changes the state of the list by pushing the text from the input field in to the array.
+    const listItemWithoutId = {
+      item: newListItem,
+      completed: false,
+    };
 
 
 
   return (
-    <div className="App">
-      
-         
-    </div>
+    <section>
+      <InputList addToList={addToList} buttonText={"Add To List"} />
+      <ShowList list={list} tickItem={tickItem} />
+      <ClearList clearList={clearList} buttonText={"Clear List"} />
+    </section>
   );
 }
 
